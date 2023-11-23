3e79e51f568bede5d963002d0548c4acb689cb31
Diego <dsepul21@gmail.com>
2023-11-23 11:30:06 -0300

    advances

--- demo/predictor.py
+++ demo/predictor.py
@@ -11,56 +11,8 @@ from detectron2.engine.defaults import DefaultPredictor
 from detectron2.utils.video_visualizer import VideoVisualizer
 from detectron2.utils.visualizer import ColorMode, Visualizer
 from sort import *
+from tools.diego_tools import draw_bboxes,save_image_based_on_sub_frame,filter_detections_inside_polygon
 
-COLORS_10 =[(144,238,144),(178, 34, 34),(221,160,221),(  0,255,  0),(  0,128,  0),(210,105, 30),(220, 20, 60),
-            (192,192,192),(255,228,196),( 50,205, 50),(139,  0,139),(100,149,237),(138, 43,226),(238,130,238),
-            (255,  0,255),(  0,100,  0),(127,255,  0),(255,  0,255),(  0,  0,205),(255,140,  0),(255,239,213),
-            (199, 21,133),(124,252,  0),(147,112,219),(106, 90,205),(176,196,222),( 65,105,225),(173,255, 47),
-            (255, 20,147),(219,112,147),(186, 85,211),(199, 21,133),(148,  0,211),(255, 99, 71),(144,238,144),
-            (255,255,  0),(230,230,250),(  0,  0,255),(128,128,  0),(189,183,107),(255,255,224),(128,128,128),
-            (105,105,105),( 64,224,208),(205,133, 63),(  0,128,128),( 72,209,204),(139, 69, 19),(255,245,238),
-            (250,240,230),(152,251,152),(  0,255,255),(135,206,235),(  0,191,255),(176,224,230),(  0,250,154),
-            (245,255,250),(240,230,140),(245,222,179),(  0,139,139),(143,188,143),(255,  0,  0),(240,128,128),
-            (102,205,170),( 60,179,113),( 46,139, 87),(165, 42, 42),(178, 34, 34),(175,238,238),(255,248,220),
-            (218,165, 32),(255,250,240),(253,245,230),(244,164, 96),(210,105, 30)]
-
-def draw_bboxes(img, bbox, offset=(0,0), num_frame=0):
-    for i,box in enumerate(bbox):
-        x1,y1,x2,y2,id, _ = [int(i) for i in box]
-        x1 += offset[0]
-        x2 += offset[0]
-        y1 += offset[1]
-        y2 += offset[1]
-        # box text and bar
-        color = COLORS_10[id%len(COLORS_10)]
-        label = '{:d} {:.2f}'.format(id,box[5])
-        t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 2 , 2)[0]
-        
-        cv2.rectangle(img,(x1, y1),(x2,y2),color,3)
-        cv2.rectangle(img,(x1, y1),(x1+t_size[0]+3,y1+t_size[1]+4), color,-1)
-        cv2.putText(img,label,(x1,y1+t_size[1]+4), cv2.FONT_HERSHEY_PLAIN, 2, [255,255,255], 2)
-    return img
-
-def save_image_based_on_sub_frame(num_frame, img, id,boxes, frame_step=10,name='images_subframe'):
-    if num_frame % frame_step == 0:
-        for i,box in enumerate(boxes):    
-            x1,y1,x2,y2,id,_ = [int(i) for i in box]
-            sub_frame = img[y1:y2,x1:x2].copy()
-
-            # Convert BGR to RGB
-            sub_frame_rgb = cv2.cvtColor(sub_frame, cv2.COLOR_BGR2RGB)
-            
-            id_directory = os.path.join(f"{name}", str(id))
-            if not os.path.exists(id_directory):
-                os.makedirs(id_directory)
-            image_name = f"img_{id}_{num_frame}_{x1}_{y1}_{x2}_{y2}_{box[5]:.2f}.png"
-            save_path = os.path.join(id_directory, image_name)
-            
-            # Save the RGB image
-            try:
-                cv2.imwrite(save_path, sub_frame_rgb)
-            except Exception as e:
-                print(f"Error encountered: {e}")
 
 class VisualizationDemo:
     def __init__(self, cfg, instance_mode=ColorMode.IMAGE, parallel=False, tracker=None):
@@ -150,7 +102,7 @@ class VisualizationDemo:
         video_visualizer = VideoVisualizer(self.metadata, self.instance_mode)
 
         def process_predictions(frame, predictions, num_frame=0):
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             if "panoptic_seg" in predictions:
                 panoptic_seg, segments_info = predictions["panoptic_seg"]
                 vis_frame = video_visualizer.draw_panoptic_seg_predictions(
@@ -160,16 +112,21 @@ class VisualizationDemo:
                 predictions = predictions["instances"].to(self.cpu_device)
 
                 dets_to_sort = torch.cat((predictions.pred_boxes.tensor,predictions.scores.unsqueeze(1), predictions.pred_classes.unsqueeze(1)),dim=1).numpy()
-                dets_to_sort = dets_to_sort[dets_to_sort[:, 5] == 0.0]
-
+                dets_to_sort = dets_to_sort[dets_to_sort[:, 5] == 0.0] #Filter only class 0
+                dets_to_sort = filter_detections_inside_polygon(detections=dets_to_sort)
                 # BYTE TRACK
-                online_targets = self.tracker.update(dets_to_sort[:,:5],frame.shape,frame.shape)
-                identities = [obj.track_id for obj in online_targets]
-                scores = [obj.score for obj in online_targets]
-                bboxes = [np.concatenate((obj.tlbr,[identity],[score])) for obj, identity, score in zip(online_targets, identities,scores)]
+                if(len(dets_to_sort) > 0):
+                    online_targets = self.tracker.update(dets_to_sort[:,:5],frame.shape,frame.shape)
+                    identities = [obj.track_id for obj in online_targets]
+                    scores = [obj.score for obj in online_targets]
+                    bboxes = [np.concatenate((obj.tlbr,[identity],[score])) for obj, identity, score in zip(online_targets, identities,scores)]            
+                    save_image_based_on_sub_frame(num_frame=num_frame,img=frame,boxes=bboxes,id=id)
+                    frame = draw_bboxes(frame,bboxes,num_frame=num_frame)
                 
-                save_image_based_on_sub_frame(num_frame=num_frame,img=frame,boxes=bboxes,id=id)
-                frame = draw_bboxes(frame,bboxes,num_frame=num_frame)
+                # For Feedback purpose
+                pts = np.array([[0,1080],[0,800],[488,561],[593,523],[603,635],[632,653],[932,535],[978,621],[756,918],[764,1080]], np.int32)
+                pts = pts.reshape((-1, 1, 2))
+                cv2.polylines(frame, [pts], isClosed=True, color=(0, 0, 255), thickness=5)
                 return frame
                 vis_frame = video_visualizer.draw_instance_predictions(frame, [])
             elif "sem_seg" in predictions:
@@ -178,7 +135,7 @@ class VisualizationDemo:
                 )
 
             # Converts Matplotlib RGB format to OpenCV BGR format
-            vis_frame = cv2.cvtColor(vis_frame.get_image(), cv2.COLOR_RGB2BGR)
+            # vis_frame = cv2.cvtColor(vis_frame.get_image(), cv2.COLOR_RGB2BGR)
             return vis_frame
 
         frame_gen = self._frame_from_video(video)
